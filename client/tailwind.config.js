0d611f88f0650791261b499ba93c4e77f428d13e
Sakshi Naravankar <126859351+sakxhii@users.noreply.github.com>
2025-02-14 09:09:09 +0530

    Add files via upload

---
+++ client/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./index.html", // For the main HTML file
+    "./src/**/*.{js,ts,jsx,tsx}", // For all JS/TS/React components in the src folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
