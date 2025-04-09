44fc3acab0a6f297f16c37b2f974e348ee70f38f
sakxhii <sakxhii>
2025-04-09 20:47:35 +0530

    finall

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -82,7 +82,7 @@ export const testimonialsData = [
 export const plans = [
     {
       id: 'Basic',
-      price: 0.1,
+      price: 1,
       credits: 200,
       desc: 'Best for personal use.'
     },
